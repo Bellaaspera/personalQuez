4308f49cd91b3ac1d7bebe2b7de0572d83b4e1f2
Bellaaspera <lanasenatorova@gmail.com>
2022-07-04 19:47:34 +0200

    Initial Commit

---
+++ personalQuez/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  personalQuez
+//
+//  Created by Светлана Сенаторова on 04.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
